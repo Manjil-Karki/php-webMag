75172950c44af5605d010d4b8edacb38ec8d770e
Manjil-Karki <kmanzil10@gmail.com>
2020-05-21 15:11:02 +0545

    secont commit

--- cms/login.php
+++ cms/login.php
@@ -57,7 +57,7 @@
 
                 <div>
                   <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                  <p>&copy; <?php echo Date('Y')?> All Rights Reserved. Gentelella Alela! Privacy and Terms</p>
                 </div>
               </div>
             </form>
